80f6eb9df8345b5e9a8380a6564eb01edd0ad934
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-07-19 20:13:57 +0100

    Section 18: Turtle Challenge 3 - refactored to use draw_shape() funciton

--- section_18/main.py
+++ section_18/main.py
@@ -2,12 +2,19 @@ from turtle import Turtle, Screen, colormode
 from random import randint
 
 tim = Turtle()
+tim.speed("slowest")
 colormode(255)
-for i in range(3, 11):
-    tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-    for j in range(i):
+
+
+def draw_shape(number_of_sides):
+    for j in range(number_of_sides):
         tim.forward(100)
-        tim.right(360 / i)
+        tim.right(360 / number_of_sides)
+
+
+for sides_number in range(3, 11):
+    tim.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
+    draw_shape(sides_number)
 
 screen = Screen()
 screen.exitonclick()
