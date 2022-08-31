8a1f8f3468fbd4303faf087037dc6a7da3fb8ce5
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2022-09-01 00:00:52 +0100

    Section 30: improved password manager.

--- section_30/password-manager/main.py
+++ section_30/password-manager/main.py
@@ -62,6 +62,23 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title="Info", message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showerror(title="Error", message=f"No details for {website} exists.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -82,16 +99,19 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=33)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=52)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "tiptil@gmail.com")
-password_entry = Entry(width=21)
+password_entry = Entry(width=33)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2)
+search_button.config(padx=33)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
