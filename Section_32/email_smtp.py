3d9cc8c97cc0e9225122cf0a86533463e5f2b4bd
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2023-04-18 20:49:43 +0100

    Section 32: Send quote from txt file via SMTP

--- Section_32/email_smtp.py
+++ Section_32/email_smtp.py
@@ -1,11 +1,26 @@
-import smtplib
+# import smtplib
+#
+# my_email = "tiptil@gmail.com"
+# my_password = "enzzutzitpwzdqto"
+# dest_email = "mic_tgv@yahoo.co.uk"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=my_password)
+#     connection.sendmail(from_addr=my_email,
+#                         to_addrs=dest_email,
+#                         msg="Subject:Python Test with not close()\n\nHello, this is an email from Python.")
 
-my_email = "tiptil@gmail.com"
-my_password = "enzzutzitpwzdqto"
-dest_email = "mic_tgv@yahoo.co.uk"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=my_password)
-connection.sendmail(from_addr=my_email, to_addrs=dest_email, msg="Hello, this is an email from Python.")
-connection.close()
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day = now.day
+day_of_week = now.weekday()
+
+DOB = dt.datetime(day=28, month=6, year=1977)
+print(DOB)
+
+print(now.year)
