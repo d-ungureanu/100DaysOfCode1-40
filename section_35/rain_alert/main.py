eed06eef9199b9f60c8baf028ae9019b91404cbf
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2023-04-25 15:37:01 +0100

    section 35: SMS app working

--- section_35/rain_alert/main.py
+++ section_35/rain_alert/main.py
@@ -1,11 +1,16 @@
 import requests
 import config
+import os
+from twilio.rest import Client
 
 api_key = config.api_key
 leeds_lat = 53.800755
 leeds_lon = -1.549077
 ozd_lat = 48.219560
 ozd_lon = 20.286920
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
 api_url = "https://api.openweathermap.org/data/2.5/forecast"
 parameters = {
@@ -28,8 +33,14 @@ will_rain = False
 
 for weather_id in weather_id_list:
     if weather_id < 700:
-        will_rain=True
+        will_rain = True
 
 if will_rain:
-    print("Bring an umbrella.")
-
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's going to rain, get an umbrella!",
+        from_='+16813076724',
+        to='+447455414104'
+    )
+print(message.status)
