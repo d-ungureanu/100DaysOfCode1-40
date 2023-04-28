22937371355ba95e03372c9894b5ac568a120512
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2023-04-28 23:19:22 +0100

    section 36: working stock alert app, my version extra hard

--- section_36/stock-news/main.py
+++ section_36/stock-news/main.py
@@ -1,6 +1,8 @@
 import requests
 import datetime as dt
 import config
+import os
+from twilio.rest import Client
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -8,19 +10,26 @@ price_variation = ""
 news_list = []
 send_sms = False
 
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
 
 def is_different(price1, price2):
     global price_variation
     global send_sms
     diff_percentage = round((price2 - price1) * 100 / price2, 2)
-    print(diff_percentage)
-    if abs(diff_percentage > 5):
+    print("% difference: ", diff_percentage)#delete
+    if abs(diff_percentage) > 5:
         get_three_headlines("Tesla Inc.")
+        print(news_list)#delete
         send_sms = True
         if diff_percentage > 0:
             price_variation += f"TSLA: 🔺{diff_percentage}%"
+            print("Variation: ", price_variation)#delete
         else:
             price_variation += f"TSLA: 🔻{diff_percentage}%"
+            print("Variation: ", price_variation)#delete
+    else:
+        print("No dif > or < than 5%")
 
 
 def get_three_headlines(news_subject):
@@ -34,13 +43,25 @@ def get_three_headlines(news_subject):
         "searchIn": "title",
         "apiKey": config.news_apiKey
     }
-
     news_raw_data = requests.get(url=news_url, params=news_parameters)
     news_data = news_raw_data.json()
     top_news = news_data["articles"][:3]
     for news_entry in top_news:
         title, text = news_entry["title"].split(": ")
         news_list.append({"title": title, "text": text})
+
+
+def send_message(newslist: list):
+    client = Client(account_sid, auth_token)
+    for index, entry in enumerate(newslist):
+        message = client.messages.create(
+            body=f"{price_variation}" + "\n" + f"Headline: {entry['title']}" + "\n" + f"Brief: {entry['text']}",
+            from_="+16813076724",
+            to="+447455414104"
+        )
+        print(f"message {index+1} status: {message.status}")
+
+
 
 
 stock_url = "https://www.alphavantage.co/query"
@@ -62,16 +83,10 @@ yesterday_price = float(yesterday_data["4. close"])
 two_days_before_data = daily_data[last_2_days[1]]
 two_days_before_price = float(two_days_before_data["4. close"])
 is_different(yesterday_price, two_days_before_price)
-print(price_variation)
 
-
-
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+print(send_sms)#delete
 if send_sms:
-    print("wohoo")
-
-
+    send_message(news_list)
 
 # Optional: Format the SMS message like this:
 """
