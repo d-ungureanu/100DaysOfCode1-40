d48275fe326c85d941238ca02709e52fcc0cfed9
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2023-04-30 20:00:56 +0100

    section 37: delete pixel

--- section_37/habit-tracker/delete_pixel.py
+++ section_37/habit-tracker/delete_pixel.py
@@ -9,20 +9,13 @@ GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-auth_header ={
+auth_header = {
     "X-USER-TOKEN": TOKEN
 }
 
-
-
-
 today = dt.datetime.today().date().strftime("%Y%m%d")
 
-pixel_update_endpoint = f"{graph_endpoint}/{GRAPH_ID}/{today}"
+pixel_delete_endpoint = f"{graph_endpoint}/{GRAPH_ID}/{today}"
 
-new_pixel_data = {
-    "quantity": "7500"
-}
-
-pixel_response = requests.put(url=pixel_update_endpoint, json=new_pixel_data, headers=auth_header)
+pixel_response = requests.delete(url=pixel_delete_endpoint, headers=auth_header)
 print(pixel_response.text)
