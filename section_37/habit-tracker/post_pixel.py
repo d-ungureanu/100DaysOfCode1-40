8c5e7c1f5569c2e16e498d5e762b2093fabd0d26
Daniel Ungureanu <leinad.ungureanu@gmail.com>
2023-04-30 19:30:07 +0100

    section 37: created GRAPH_ID constant

--- section_37/habit-tracker/post_pixel.py
+++ section_37/habit-tracker/post_pixel.py
@@ -4,6 +4,7 @@ import config
 
 USERNAME = "tiptil"
 TOKEN = config.pixela_token
+GRAPH_ID = "graph1"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
@@ -13,7 +14,7 @@ auth_header ={
 }
 
 
-pixel_endpoint = f"{graph_endpoint}/graph1"
+pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
 
 today = dt.datetime.today().date().strftime("%Y%m%d")
 pixel_config = {
